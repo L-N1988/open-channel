546216c828f145037f7038aed10808c2933dff0f
L-N1988 <2994567011@qq.com>
2023-11-11 16:26:43 +0800

    lorenz attractor.

--- docs/img/lorenz.py
+++ docs/img/lorenz.py
@@ -16,7 +16,7 @@ sigma, beta, rho = 10, 2.667, 28
 u0, v0, w0 = 0, 1, 1.05
 
 # Maximum time point and total number of time points.
-tmax, n = 100, 100000
+tmax, n = 100, 400000
 
 def lorenz(t, X, sigma, beta, rho):
     """The Lorenz equations."""
@@ -34,10 +34,11 @@ t = np.linspace(0, tmax, n)
 x, y, z = soln.sol(t)
 
 # Plot the Lorenz attractor using a Matplotlib 3D projection.
-fig = plt.figure(facecolor='k', figsize=(WIDTH/DPI, HEIGHT/DPI))
+# fig = plt.figure(facecolor='k', figsize=(WIDTH/DPI, HEIGHT/DPI))
+fig = plt.figure()
 # ax = fig.gca(projection='3d')
 ax = fig.add_subplot(projection = '3d')
-# ax.set_facecolor('k')
+ax.set_facecolor('w')
 fig.subplots_adjust(left=0, right=1, bottom=0, top=1)
 
 # Make the line multi-coloured by plotting it in segments of length s which
--- docs/index.md
+++ docs/index.md
@@ -1,8 +1,5 @@
 
 欢迎来到**了那**的 WiKi.
 
+<img src="img/shotfig.png" alt="drawing" width="600"/>
 
-<figure markdown>
-  ![lorenz](img/shotfig.png)
-  <figcaption>Fig 1: The Lorenz attractor </figcaption>
-</figure>
